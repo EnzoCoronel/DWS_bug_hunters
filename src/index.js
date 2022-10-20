f93bffee04d63de170b07a0f3ecec6c022875a56
Enzo Coronel <enzocoronel1000@gmail.com>
2022-10-20 10:16:22 -0300

    factions_obtained

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import read from "../resources/read.js";
+import axios from "axios";
 
 //your code goes here;
 const menu = async () => {
-  let choice = 2;
-  while (choice != "0") {
-    console.log("1 - Create Character\n 2- Log in\n 0 - Sair");
-    choice = await read("Sua escolha:");
+  const getFac = async (fac) => {
+    const response = (
+      await axios.get("https://ir39vnlo.directus.app/items/factions")
+    ).data;
+    return response;
+  }
 
+  let choice = 2;
+
+  while (choice != "0") {
+    choice = await read("1 - Create Character\n 2- Log in\n 0 - Exit");
     switch (choice) {
       case "1":
-        const nome = await read("Digite seu nome: "); //example for read user input
+        try {
+          const factions = await getFac("fac");
+          console.log(factions);
+        } catch (error) {
+          console.log(error);
+        }
+        const nome = await read("Type your name: "); //example for read user input
         console.log(`Olá ${nome}`);
         break;
       case "2":
         console.log("No data");
         break;
       case "0":
-        console.log("Vamos sentir saudades!");
+        console.log("We will miss you!");
         break;
       default:
         console.error("Chose a valid option");
