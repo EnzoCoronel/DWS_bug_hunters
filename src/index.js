c3bf9d9a3d791a3f329e1c63eb5aaa0dfda094f6
Enzo Coronel <enzocoronel1000@gmail.com>
2022-10-20 09:25:49 -0300

    menu_completed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import read from "../resources/read.js";
 
 //your code goes here;
 const menu = async () => {
-  const nome = await read("Digite seu nome: "); //example for read user input
-  console.log(`Olá ${nome}`);
+  let choice = 2;
+  while (choice != "0") {
+    console.log("1 - Create Character\n 2- Log in\n 0 - Sair");
+    choice = await read("Sua escolha:");
+
+    switch (choice) {
+      case "1":
+        const nome = await read("Digite seu nome: "); //example for read user input
+        console.log(`Olá ${nome}`);
+        break;
+      case "2":
+        console.log("No data");
+        break;
+      case "0":
+        console.log("Vamos sentir saudades!");
+        break;
+      default:
+        console.error("Chose a valid option");
+        break;
+    }
+  }
 };
 
 export default menu;
