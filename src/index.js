b2bb3b31a8f2f04139164448ff643bcb80e7a7d5
Enzo Coronel <enzocoronel1000@gmail.com>
2022-10-20 11:23:16 -0300

    All_english

--- src/index.js
+++ src/index.js
@@ -4,19 +4,17 @@ import read from "../resources/read.js";
 const menu = async () => {
   let choice = 2;
   while (choice != "0") {
-    console.log("1 - Create Character\n 2- Log in\n 0 - Sair");
-    choice = await read("Sua escolha:");
-
+    choice = await read("1 - Create Character\n 2- Log in\n 0 - Exit");
     switch (choice) {
       case "1":
-        const nome = await read("Digite seu nome: "); //example for read user input
-        console.log(`Olá ${nome}`);
+        const name = await read("Type your name"); //example for read user input
+        console.log(`Hello ${name}`);
         break;
       case "2":
         console.log("No data");
         break;
       case "0":
-        console.log("Vamos sentir saudades!");
+        console.log("We will miss you!");
         break;
       default:
         console.error("Chose a valid option");
