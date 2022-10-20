eb8249539f998823cbdb28cd0bd48d9a14ac8941
Enzo Coronel <enzocoronel1000@gmail.com>
2022-10-20 17:16:41 -0300

    add_Json

--- src/index.js
+++ src/index.js
@@ -9,10 +9,33 @@ const menu = async () => {
     return response;
   };
 
-  const postApi = async (char, item) => {
-    const response = (await axios.post(api + item).data, {
-      name: username,
-      factions: userFaction
+  const postApi = async (userName, userFaction, fac_id, item) => {
+    const response = (await axios.post(api + item).data,  {
+      "atk": 10,
+      "def": 10,
+      "agi": 10,
+      "hp": 10,
+      "gold": 100,
+      "name": userName,
+      "factions": [
+        {
+          "factions_id": {
+            "id": fac_id,
+            "name": userFaction
+          }
+        }
+      ],
+      "equipments": [
+        {
+          "equipments_id": {
+            "id": 4,
+            "name": "Excalibug",
+            "affected_attribute": "atk",
+            "affected_amount": 50,
+            "value": 5000
+          }
+        }
+      ]
     })
     return response;
   };
@@ -39,28 +62,28 @@ const menu = async () => {
         console.log("Tell me traveller, who's side are you on?");
         const factions = await printFactions();
         const numFaction = await read("");
-        let userFaction = '';
+        let faction = '';
         switch (numFaction) {
           case "1":
-            userFaction = 'Frontend';
+            faction = 'Frontend';
             break;
             case "2":
-              userFaction = 'Backend';
+              faction = 'Backend';
               break;
             case "13":
-              userFaction = 'Mobile';
+              faction = 'Mobile';
             break;
           default:
             console.error("Chose a valid option");
             break;
         }
-        console.log(`Welcome ${userFaction} dev!`);
+        console.log(`Welcome ${faction} dev!`);
 
         const name = await read("What is your name?\n"); //example for read user input
         console.log(`Nice to meet you ${name}!`);
 
         try{
-          const print = await postApi("characters?fields=*,equipments.equipments_id.*,factions.factions_id.*")
+          const print = await postApi(name, faction, numFaction,"characters?fields=*,equipments.equipments_id.*,factions.factions_id.*")
           console.log(print);
         } catch (error){
           console.log(error);
