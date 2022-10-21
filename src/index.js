c29f29423a53dfbe8f59d36426cff71ca81a7014
Enzo Coronel <enzocoronel1000@gmail.com>
2022-10-21 13:28:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ const menu = async () => {
   };
 
   const postApi = async (character, item) => {
-    const response = await axios.post (api + item, character)
+    const response = await axios.post(api + item, character);
     return response;
   };
 
   const getFactions = async () => {
     try {
       let rawFac = await getApi("factions");
-      const sortedFac = rawFac.map(element => {
+      const sortedFac = rawFac.map((element) => {
         //console.log(`${element.id} - ${element.name}`)
-        const {id, name} = element;
-        return {id, name};
+        const { id, name } = element;
+        return { id, name };
       });
       return sortedFac;
     } catch (error) {
@@ -28,74 +28,87 @@ const menu = async () => {
     }
   };
 
-  let choice;
+  const search = async () => {
+    try{
+      
+    } catch (error){
+      console.log(error);
+    }
+  }
+
+  console.log("DWS BUG HUNTERS\n");
+
+  let choice, name;
 
   while (choice != "0") {
-    choice = await read("1 - Create Character\n 2 - Log in\n 0 - Exit");
+    choice = await read("\n1 - Create Character\n 2 - Log in\n 0 - Exit");
     switch (choice) {
       case "1":
         console.log("Tell me traveller, who's side are you on?");
         const factions = await getFactions();
         factions.forEach((element, index) => {
-          console.log(index + " - " + element.name);
+          console.log(`${index + 1} - ${element.name}`);
         });
 
         let faction;
         let facId;
         let facChoice = await read("");
-        switch (facChoice) {
-          case "0":
-            faction = factions[0].name;
-            facId = factions[0].id;
-            break;
+        while (facChoice < 1 && facChoice > 3) {
+          switch (facChoice) {
             case "1":
+              faction = factions[0].name;
+              facId = factions[0].id;
+              break;
+            case "2":
               faction = factions[1].name;
               facId = factions[1].id;
               break;
-            case "2":
+            case "3":
               faction = factions[2].name;
               facId = factions[2].id;
-            break;
-          default:
-            console.error("Chose a valid option");
-            break;
+              break;
+            default:
+              console.error("Chose a valid option.");
+              break;
+          }
         }
         console.log(`Welcome ${faction} dev!`);
 
-        const name = await read("What is your name?\n"); //example for read user input
+        name = await read("What is your name?\n"); //example for read user input
         console.log(`Nice to meet you ${name}!`);
         let user = {
-          "atk": 10,
-          "def": 10,
-          "agi": 10,
-          "hp": 10,
-          "gold": 100,
-          "name": name,
-          "factions": [
+          atk: 10,
+          def: 10,
+          agi: 10,
+          hp: 10,
+          gold: 100,
+          name: name,
+          factions: [
             {
-              "factions_id": {
-                "id": facId,
-                "name": faction
-              }
-            }
+              factions_id: {
+                id: facId,
+                name: faction,
+              },
+            },
           ],
-          "equipments": []
+          equipments: [],
         };
-        try{
-          const print = await postApi(user, "characters?fields=*,equipments.equipments_id.*,factions.factions_id.*")
+        try {
+          const print = name; //await postApi(user, "characters?fields=*,equipments.equipments_id.*,factions.factions_id.*")
           console.log(print);
-        } catch (error){
+        } catch (error) {
           console.log(error);
         }
         break;
       case "2":
-        console.log("No data");
+        name = await read("Do i remember you? Tell me your name.");
+
         break;
       case "0":
         console.log("We will miss you!");
         break;
       default:
-        console.error("Chose a valid option");
+        console.error("Chose a valid option.");
         break;
     }
   }
