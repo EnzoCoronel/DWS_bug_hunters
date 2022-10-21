8c3c018a52b0ca1d7ce6e77b7191437e1ed767cd
Enzo Coronel <enzocoronel1000@gmail.com>
2022-10-21 11:31:40 -0300

    working_post

--- src/index.js
+++ src/index.js
@@ -9,69 +9,52 @@ const menu = async () => {
     return response;
   };
 
-  const postApi = async (userName, userFaction, fac_id, item) => {
-    const response = (await axios.post(api + item).data,  {
-      "atk": 10,
-      "def": 10,
-      "agi": 10,
-      "hp": 10,
-      "gold": 100,
-      "name": userName,
-      "factions": [
-        {
-          "factions_id": {
-            "id": fac_id,
-            "name": userFaction
-          }
-        }
-      ],
-      "equipments": [
-        {
-          "equipments_id": {
-            "id": 4,
-            "name": "Excalibug",
-            "affected_attribute": "atk",
-            "affected_amount": 50,
-            "value": 5000
-          }
-        }
-      ]
-    })
+  const postApi = async (character, item) => {
+    const response = await axios.post (api + item, character)
     return response;
   };
 
-  const printFactions = async () => {
+  const getFactions = async () => {
     try {
-      const rawFac = await getApi("factions");
-      rawFac.forEach(element => {
-        console.log(`${element.id} - ${element.name}`)
+      let rawFac = await getApi("factions");
+      const sortedFac = rawFac.map(element => {
+        //console.log(`${element.id} - ${element.name}`)
+        const {id, name} = element;
+        return {id, name};
       });
-      console.log(rawFac);
-      return rawFac;
+      return sortedFac;
     } catch (error) {
       console.log(error);
     }
   };
 
-  let choice = 2;
+  let choice;
 
   while (choice != "0") {
     choice = await read("1 - Create Character\n 2 - Log in\n 0 - Exit");
     switch (choice) {
       case "1":
         console.log("Tell me traveller, who's side are you on?");
-        const factions = await printFactions();
-        const numFaction = await read("");
-        let faction = '';
-        switch (numFaction) {
-          case "1":
-            faction = 'Frontend';
+        const factions = await getFactions();
+        factions.forEach((element, index) => {
+          console.log(index + " - " + element.name);
+        });
+
+        let faction;
+        let facId;
+        let facChoice = await read("");
+        switch (facChoice) {
+          case "0":
+            faction = factions[0].name;
+            facId = factions[0].id;
             break;
-            case "2":
-              faction = 'Backend';
+            case "1":
+              faction = factions[1].name;
+              facId = factions[1].id;
               break;
-            case "13":
-              faction = 'Mobile';
+            case "2":
+              faction = factions[2].name;
+              facId = factions[2].id;
             break;
           default:
             console.error("Chose a valid option");
@@ -81,9 +64,25 @@ const menu = async () => {
 
         const name = await read("What is your name?\n"); //example for read user input
         console.log(`Nice to meet you ${name}!`);
-
+        let user = {
+          "atk": 10,
+          "def": 10,
+          "agi": 10,
+          "hp": 10,
+          "gold": 100,
+          "name": name,
+          "factions": [
+            {
+              "factions_id": {
+                "id": facId,
+                "name": faction
+              }
+            }
+          ],
+          "equipments": []
+        };
         try{
-          const print = await postApi(name, faction, numFaction,"characters?fields=*,equipments.equipments_id.*,factions.factions_id.*")
+          const print = await postApi(user, "characters?fields=*,equipments.equipments_id.*,factions.factions_id.*")
           console.log(print);
         } catch (error){
           console.log(error);
@@ -103,4 +102,3 @@ const menu = async () => {
 };
 
 export default menu;
-
